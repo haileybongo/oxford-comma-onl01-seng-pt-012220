1ea95ddfd950d2d57eb3307c455b0b305b0ac915
Hailey Bongo <haileyebongo@gmail.com>
2020-01-29 04:28:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@
 def oxford_comma(array)
   oxford = ""
   if array.size == 2 
-    oxford << "#{array [0]}"
-    oxford << "and"
+    oxford << "#{array [0]} "
+    oxford << "and "
     oxford << "#{array[1]}"
   elsif array.size > 2 
     j=0
