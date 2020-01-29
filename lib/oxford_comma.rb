d225d5ccb2b805f08ae974f5fec4adc4c6bcb80d
Hailey Bongo <haileyebongo@gmail.com>
2020-01-29 04:22:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,12 @@ def oxford_comma(array)
     oxford << "and"
     oxford << "#{array[1]}"
   elsif array.size > 2 
+    j=0
+    until j = array.size-2
+      oxford << "#{[array[j], }"
+      
+    end
+    oxford << "#{[array["
     i = 2
     j = 0
     while array.size < i 
