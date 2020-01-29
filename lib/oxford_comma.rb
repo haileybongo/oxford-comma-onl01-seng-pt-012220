6eea6ca144a954f4415dc1c56785db00f98367ab
Hailey Bongo <haileyebongo@gmail.com>
2020-01-29 03:40:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
 def oxford_comma(array)
-
+  #array.join(", ")
+ i = 1
+ new_array = []
+  array.each do |item|
+    if array.size = 2 
+      new_array <<"#{item}, "
+      i =+ 1
+    else 
+      new_array << "and #{item}"
 end
+return item
+end
+end
+
+
+def oxford_comma(array)
+  oxford = []
+  if array.size = 2 
+    oxford << "#{array [0]}"
+    oxford << "and"
+    oxford << "#{array[1]}"
+  elsif
+    array.size
+    
